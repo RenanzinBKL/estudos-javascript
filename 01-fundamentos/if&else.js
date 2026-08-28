cd639eba51cdab048f0ff31c7163301ff60daf2f
Renan Felipe <84744635+RenanzinBKL@users.noreply.github.com>
2026-08-28 02:18:31 -0300

    Add files via upload

---
+++ 01-fundamentos/if&else.js
@@ -0,0 +1,13 @@
+// If & Else: o if é o 'Se' e o else é o 'Se não', ou seja, se aconteçer tal coisa faça isso e se não aconteçer faça aquilo.
+
+// Saber se o aluno está aprovado ou reprovado somente com if e else, sempre usar letra minuscula.
+const aluno = 8
+
+// Faça assim: primeiro vem o if, depois vem a condição () e depois vem o que vai aconteçer {}, o else sempre vem quando nada der certo, ou seja é caso o if não de certo já vai direto e nn precisa declarar.
+if(aluno >= 7){
+    console.log('Aluno Aprovado.')
+} else{
+    console.log('Aluno Reprovado.')
+}
+
+// Temos tambem o ifelse que funciona como uma condicional, caso não seja if, pode ser ifelse e caso nn seja nenhum vai pra else.
