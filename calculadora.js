19f6eea436314a77dc304c74c142144bc9f29a18
Renan Felipe <84744635+RenanzinBKL@users.noreply.github.com>
2026-08-28 02:10:39 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+// Calculadora - 28/08/2026
+function calculadora(a, b, operacao) {
+    if(operacao == '+'){
+        console.log(a + b)
+    } else if(operacao == '-'){
+        console.log(a - b)
+    } else if(operacao == '*'){
+        console.log(a * b)
+    } else if(operacao == '/'){
+        console.log(a / b)
+    }
+}
+
+calculadora(10, 8, '-')
