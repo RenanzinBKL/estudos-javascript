67e52cd5b105495c4bb2d4179a5d83455ef77fab
Renan Felipe <84744635+RenanzinBKL@users.noreply.github.com>
2026-08-28 02:16:45 -0300

    Add files via upload

---
+++ 01-fundamentos/funcoes/funcoes-e-parametros.js
@@ -0,0 +1,20 @@
+// Função é seu cunhado legal, ou seja ele espera pra ser chamado, vc cria ele e quando precisar chamar é so chamar.
+
+function digaMeuNome() {
+    console.log('Renan Felipe')
+}
+
+// Para chamar a função e fazer ela funcionar se usa o nome da função e os ().
+digaMeuNome()
+
+// Agora vamos pra Parametro, ou seja eu preciso jogar alguma informação pra ela funcionar.
+
+function digaQualquerNome(nome){
+
+    console.log(nome)
+}
+
+digaQualquerNome('Renan')
+
+// Funções com parametros tbm podem ser usados para calculos, exemplo funcion soma(numero1, numero2) {const resultado = numero1 + numero2    console.log(resultado)} ai na hora de chamar usa soma(3,6) por exemplo.
+
