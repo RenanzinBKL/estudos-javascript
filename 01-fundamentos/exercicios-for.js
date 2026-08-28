987d2222f89740e9a360816cc29ac1e292b107ad
Renan Felipe <84744635+RenanzinBKL@users.noreply.github.com>
2026-08-28 04:35:27 -0300

    Add files via upload

---
+++ 01-fundamentos/exercicios-for.js
@@ -0,0 +1,11 @@
+// O for é uma estrutura de repetição, um pedaço de codigo que vai repetir quantas vezes vc quiser.
+ for(let i = 0; i < 10; i++){
+    console.log(i)
+ }
+
+// Outro lugar que ele é útil:
+const users = ['Maria', 'Renan', 'Rebeca', 'João']
+
+for(let i = 0; i < users.length; i++){
+   console.log(users[i])
+}
